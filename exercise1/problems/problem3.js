1471b198e74916e3c16224bd0a9b01ec796c26cd
Laurens Inauen <laurensinauen@gsnail.ch>
2026-03-18 00:54:24 +0100

    Update exercises

--- exercise1/problems/problem3.js
+++ exercise1/problems/problem3.js
@@ -1,2 +1,8 @@
 // create and export an object called note with the properties: pitch, velocity and duration
 // the values of these properties can be any sensible number
+
+export const note = {
+    pitch: 220,
+    duration: 300,
+    velocity: 2000,
+}
---
+++ exercise3/src/index.js
@@ -0,0 +1,17 @@
+// study the html file and then solve the following problem
+
+// problem 1
+// grab the button with the text "Play"
+// when it is clicked start/resume an audio context and play an oscillator
+
+// problem 2
+// grab the input element and use its value to control the volume of your oscillator
+// you will have to create a gain node for this and route the oscillator through the gain node
+
+// problem 3
+// grab the button with the text "Reset"
+// reset the value of the input element to 0 everytime the button is pressed, make sure that the volume also correctly resets to 0
+
+// problem 4
+// add another input slider and control and use it to control the frequency of the oscillator
+// you need to add it in the .html file first
